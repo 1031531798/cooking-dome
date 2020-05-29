e6517c72d9584f6845ece3d7552686c7c64eb542
1031531798 <1031531798@qq.com>
2020-05-29 09:41:21 +0800

    22

--- src/components/userIcon.vue
+++ src/components/userIcon.vue
@@ -1,85 +1,33 @@
 <template>
-  <div id="indexTop">
-    <img src="../assets/mark.jpg" alt="舌尖上的中国" title="舌尖上的中国" />
-    <div id="input">
-        <i-input size="large" :placeholder="msg" style="width:200%;">
-        <i-select slot="prepend" style="width: 5rem" :model.sync="model1" placeholder="全部菜系">
-          <i-option v-for="item in cityList" :value="item.value">{{ item.label }}</i-option>
-        </i-select>
-        <i-button slot="append" icon="ios-search"></i-button>
-      </i-input>
+  <div id="userIcon">
+    <div class="userImger">
+      <img :src="userImgerSrc" alt />
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "indexTop",
+  name: "userIcon",
   data() {
     return {
-      msg: "开始搜索各式各样的菜肴吧！",
-      cityList: [
-        {
-          value: "chuancai",
-          label: "川菜"
-        },
-        {
-          value: "lucai",
-          label: "鲁菜"
-        },
-        {
-          value: "yuecai",
-          label: "粤菜"
-        },
-        {
-          value: "sucai",
-          label: "苏菜"
-        },
-        {
-          value: "zhecai",
-          label: "浙菜"
-        },
-        {
-          value: "mincai",
-          label: "闽菜"
-        },
-        {
-          value: "xiangcai",
-          label: "湘菜"
-        },
-        {
-          value: "huicai",
-          label: "徽菜"
-        }
-      ],
-      model1: ""
+      model1: "",
+      userImageSrc: "../assets/cat3.jpg"
     };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#indexTop {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  height: 10%;
-}
-#input {
-  display: flex;
-  width: 60%;
-}
-img {
-  width: 10rem;
-  height: 5rem;
-  margin-right: 2rem;
-}
-#user_love {
-  font-size: 2rem;
-}
-span{
-  position: relative;
-  display: flex;
-}
+  .userImger {
+    width: 2rem;
+    height: 2rem;
+    border-radius: 1rem;
+  }
+  .userImger img{
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
 </style>
